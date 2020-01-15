4b534ead72bcfff2c3fa0d79a2213d9e34ac1234
Artem <>
2020-01-15 15:12:14 +0500

    update

---
+++ lesson/item1.py
@@ -0,0 +1,16 @@
+# Имена переменных могут состоять только из букв, цифр и символов подчёркивания.
+	# Пробелы запрещены
+	# Имя не может начинаться с цифры
+	# Не используем именя функций и ключевые слова Python в качестве имён переменных  (другими слова не используйте слова, которые зарезервированы в Python для конкретной цели, пример: print)
+
+message = 'Hello world';
+print(message);
+
+message_1 = 'Hello world';
+print(message_1);
+
+message_text = 'Hello world';
+print(message_text);
+
+messageText = 'Hello world';
+print(messageText);
